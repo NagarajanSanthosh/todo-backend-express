64fdd4ee90afcd7ce3a1896e5166c320b88fbd63
Santhosh Nagarajan <110588320+NagarajanSanthosh@users.noreply.github.com>
2024-04-11 02:28:47 +0530

    backend uploaded

---
+++ backend/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const uri = 'mongodb+srv://64208ns:ky1B9hLuegbB3TWI@todotasks.qsqksr2.mongodb.net/todo?retryWrites=true&w=majority&appName=todotasks'
+mongoose.connect(uri).then(app.listen(5555, () => {
+    console.log("Server is running on port 5555");
+})).catch(err => console.log("db not connected", err));
+// mongoose.connect('mongodb://localhost:27017/todo')
+const userS = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+const taskS = new mongoose.Schema({
+    id: Number,
+    text: String,
+    description: String,
+    completed: Boolean,
+})
+const taskModel = mongoose.model('tasks', taskS)
+const userModel = mongoose.model('userslist', userS);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Home');
+});
+
+app.post('/api/signup', async (req, res) => {
+    const { name, email, password } = req.body;
+    const data = {
+        "name": name,
+        "email": email,
+        "password": password
+    };
+    console.log(data);
+    try {
+        await userModel.create(data);
+        res.status(201).send("User created successfully");
+    } catch (error) {
+        console.error('Error occurred', error);
+        res.status(500).send("Error creating user");
+    }
+});
+app.post('/api/tasks', async (req, res) => {
+    const { id, text, description, completed } = req.body;
+    const data = {
+        id: id,
+        text: text,
+        description: description,
+        completed: completed,
+    }
+    try {
+        await taskModel.create(data);
+        res.status(201).send("Task uploaded");
+    }
+    catch (error) {
+        console.error('Error occurred', error);
+        res.status(500).send("Error creating task");
+    }
+})
+app.get('/api/tasks', (req, res) => {
+    taskModel.find()
+        .then(tasks => res.json(tasks))
+        .catch(err => res.json(err));
+})
+
